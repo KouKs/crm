30ee632e31e232c94b1256ae84033d6bd0f9deb8
Pavel Koch <kouks.koch@gmail.com>
2016-07-12 19:15:08 +0200

    Empty plugin
    Empty Basic CRM plugin. Contains plugin header and direct call
    restriction

---
+++ wp-content/plugins/basiccrm/BasicCRM.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package CRM
+ */
+
+/**
+ * Plugin Name: Basic CRM
+ * Description: Simple CROD system.
+ * Version: 1.0
+ * Author: Pavel Koch
+ * Text Domain: basiccrm
+ */
+
+// Make sure we don't expose any info if called directly
+defined('ABSPATH') or die('No script kiddies please!'); 
+
+// Defining some useful constants
+define('CRM_VERSION', '1.0');
+define('CRM_PLUGIN_DIR', plugin_dir_path(__FILE__));
+
+
