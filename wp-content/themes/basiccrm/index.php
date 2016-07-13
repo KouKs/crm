2c6424ffe551a0a8ae9dff66ab5b164de8878dcb
Pavel Koch <kouks.koch@gmail.com>
2016-07-13 15:23:35 +0200

    Custom search
    Added searching by company and last name

--- wp-content/themes/basiccrm/index.php
+++ wp-content/themes/basiccrm/index.php
@@ -2,33 +2,37 @@
 /**
  * The main template file
  */
-$orderby = @$_GET['orderby'] ? $_GET['orderby'] : '';
-$order = @$_GET['order'] ? $_GET['order'] : '';
-
-$loop = new WP_Query([
-	'post_type' => 'client',
-	'orderby' => $orderby,
-	'order' => $order,
-]);
-
-$reverse = [ 'asc' => 'desc', 'desc' => 'asc' ];
 
 get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			<?php if ($loop->have_posts()) : ?>
+
+			<form role="search" method="get" id="searchform" class="search" action="http://localhost/crm/">
+				<div>
+					<input type="text" name="s" id="s" placeholder="Search">
+					<select name="column">
+						<option value="_crm_last_name">In last names</option>
+						<option value="_crm_company">In companies</option>
+					</select>
+					<button>Go</button>
+				</div>
+			</form>
+
+			<?php if (have_posts()) : ?>
+
+
 
 				<table id="clients" class="table-simple">
 					<thead>
 						<tr>
-							<td class="sortable <?= $orderby == '_crm_last_name' ? $order : 'none' ?>">
-								<a href="<?= esc_url(add_query_arg([ 'orderby' => '_crm_last_name', 'order' => $orderby == '_crm_last_name' ? $reverse[$order] : 'asc', ], $_SERVER['REQUEST_URI'])); ?>">
+							<td class="sortable <?= get_order_class('_crm_last_name') ?>">
+								<a href="<?= get_order_url('_crm_last_name') ?>">
 									<span>Name</span>									
 								</a>
 							</td>
-							<td class="sortable <?= $orderby == '_crm_company' ? $order : 'none' ?>">
-								<a href="<?= esc_url(add_query_arg([ 'orderby' => '_crm_company', 'order' => $orderby == '_crm_company' ? $reverse[$order] : 'asc', ], $_SERVER['REQUEST_URI'])); ?>">
+							<td class="sortable <?= get_order_class('_crm_company') ?>">
+								<a href="<?= get_order_url('_crm_company') ?>">
 									<span>Position</span>									
 								</a>
 							</td>
@@ -37,8 +41,25 @@ get_header(); ?>
 						</tr>
 					</thead>
 
+					<tfoot>
+						<tr>
+							<td class="sortable <?= get_order_class('_crm_last_name') ?>">
+								<a href="<?= get_order_url('_crm_last_name') ?>">
+									<span>Name</span>									
+								</a>
+							</td>
+							<td class="sortable <?= get_order_class('_crm_company') ?>">
+								<a href="<?= get_order_url('_crm_company') ?>">
+									<span>Position</span>									
+								</a>
+							</td>
+							<td>Contact</td>
+							<td>Date</td>
+						</tr>
+					</tfoot>
+
 					<tbody>
-						<?php while ($loop->have_posts()) : $loop->the_post();
+						<?php while (have_posts()) : the_post();
 
 							$companies = get_the_terms($post, 'company');
 							$positions = get_the_terms($post, 'position');
@@ -64,10 +85,24 @@ get_header(); ?>
 										get_the_modified_date()); ?>
 								</td>
 							</tr>
+
+							<?php 
+								wp_link_pages([
+									'before'      => '<div class="page-links"><span class="page-links-title">Pages</span>',
+									'after'       => '</div>',
+									'link_before' => '<span>',
+									'link_after'  => '</span>',
+									'pagelink'    => '<span class="screen-reader-text">Page </span>%',
+									'separator'   => '<span class="screen-reader-text">, </span>',
+								    'nextpagelink'      => 'Next &raquo',
+								    'previouspagelink'  => '&laquo Previous',
+								]);
+							?>
 						<?php endwhile; ?>
 					</tbody>
 
 				</table>
+
 
 			<?php endif; ?>
 
